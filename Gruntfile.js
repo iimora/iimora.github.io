2b844e18417917eb428952611ba73ae1fbd2d143
Mike Rapadas <mike@mrap.me>
2018-08-19 10:16:48 -0700

    Initial boiler plate / MVP

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    sass: {
+      main: {
+        files: {
+          'main.css': 'src/stylesheets/main.scss'
+        },
+      },
+    },
+
+    autoprefixer: {
+      main: {
+        src: 'main.css'
+      }
+    },
+
+    useminPrepare: {
+      html: 'src/index.html',
+      options: {
+        root: './',
+        dest: './',
+      },
+    },
+
+    copy: {
+      index: {
+        src: 'src/index.html',
+        dest: 'index.html'
+      }
+    },
+
+    cssmin: {
+      options: {
+        report: 'gzip'
+      },
+      main: {
+        dest: 'main.css',
+        src: 'main.css'
+      }
+    },
+
+    usemin: {
+      html: 'index.html',
+      options: {
+        assetsDirs: ['src']
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      gruntfile: {
+        files: ['Gruntfile.js'],
+        tasks: ['buildDev'],
+      },
+      assets: {
+        files: ['src/**/*'],
+        tasks: ['buildDev'],
+      },
+    }
+
+  });
+
+  grunt.registerTask('default', ['buildDev']);
+  grunt.registerTask('mainCSS', ['sass', 'autoprefixer']);
+
+  grunt.registerTask('buildDev', [
+    'useminPrepare',
+    'mainCSS',
+    'concat:generated',
+    'cssmin:generated',
+    // 'uglify:generated',
+    'copy:index',
+    'usemin'
+  ]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# iimora.github.io
-Official IIMORA Website
+### Setup
+
+`npm install`
+
+### Developing
+
+Work on files in `src/` and compile with `grunt`.
+Run `grunt` to build compiled assets in the root dir.
+
+`grunt`
+
+During active development, you'll want to compile as you edit files so you can see your changes.
+
+`grunt watch`
